d49bca9bd01222bd4cced912ebe4514750675062
agripaa <agrieva.xananda08@gmail.com>
2025-05-31 14:35:16 +0700

    update

--- src/modules/user/user.route.js
+++ src/modules/user/user.route.js
@@ -4,25 +4,40 @@ const router = express.Router();
 const UserController = require('./user.controller');
 
 const asyncErrorHandler = require('../../errors/asyncErrorHandler');
+const { 
+    idParamValidator, 
+    updateUserValidator,
+    createUserValidator
+} = require('./user.validator');
+const validateRequest = require('../../middlewares/validation.middleware');
 
 router.get('/', asyncErrorHandler(
     UserController.getAll.bind(UserController)
 ));
 
-router.get('/:id', asyncErrorHandler(
-    UserController.getById.bind(UserController)
-));
+router.get('/:id', 
+    idParamValidator,
+    validateRequest,
+    asyncErrorHandler(UserController.getById.bind(UserController))
+);
 
-router.post('/', asyncErrorHandler(
-    UserController.create.bind(UserController)
-));
+router.post('/', 
+    createUserValidator,
+    validateRequest,
+    asyncErrorHandler(UserController.create.bind(UserController))
+);
 
-router.put('/:id', asyncErrorHandler(
-    UserController.update.bind(UserController)
-));
+router.put('/:id', 
+    idParamValidator,
+    updateUserValidator,
+    validateRequest,
+    asyncErrorHandler(UserController.update.bind(UserController))
+);
 
-router.delete('/:id', asyncErrorHandler(
-    UserController.delete.bind(UserController)
-));
+router.delete('/:id', 
+    idParamValidator,
+    validateRequest,
+    asyncErrorHandler(UserController.delete.bind(UserController))
+);
 
 module.exports = router;
