214799d395564f31800c4ab3e4f6bce0a291e1ee
agripaa <agrieva.xananda08@gmail.com>
2025-05-31 14:26:47 +0700

    update

--- src/modules/user/user.route.js
+++ src/modules/user/user.route.js
@@ -9,4 +9,20 @@ router.get('/', asyncErrorHandler(
     UserController.getAll.bind(UserController)
 ));
 
+router.get('/:id', asyncErrorHandler(
+    UserController.getById.bind(UserController)
+));
+
+router.post('/', asyncErrorHandler(
+    UserController.create.bind(UserController)
+));
+
+router.put('/:id', asyncErrorHandler(
+    UserController.update.bind(UserController)
+));
+
+router.delete('/:id', asyncErrorHandler(
+    UserController.delete.bind(UserController)
+));
+
 module.exports = router;
